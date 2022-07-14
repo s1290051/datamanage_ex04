146b326abf6ab729315aeeb5e91840c1c063ccc7
KOISO Naoki <s1290051@u-aizu.ac.jp>
2022-07-14 17:35:29 +0900

    Victory message added

--- ex04.java
+++ ex04.java
@@ -1,4 +1,6 @@
 import java.util.Random;
+import java.util.Scanner;
+
 
 public class ex04
 {
@@ -8,12 +10,25 @@ public class ex04
 	String[] head = {"Heads","Tails"};
 
 	Random ran = new Random();
+
+
+	Scanner sc = new Scanner(System.in);
 	
 	int value;
 	value = 0;
 
 
+	System.out.println("Who are you?");
+	System.out.print(">");
+
+
+	String NAME = sc.nextLine();
+
+
+	System.out.println("Hello, " + NAME + "!");
+
 	System.out.println("Tossing a coin...");
+	
 	int x;
 	
 	
@@ -35,4 +50,14 @@ public class ex04
 	int number;
 	number = 3-value;
 
-	System.out.println("Heads:" + value + ", Tails:" + number);}}
+
+
+
+
+	System.out.println("Heads:" + value + ", Tails:" + number);
+
+	if(value > number)
+	    {System.out.println(NAME + " won!");}
+
+	if(value < number)
+	    {System.out.println(NAME + " lost.");}}}
